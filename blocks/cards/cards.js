092de29b3f9f4075c06fc1a7bf2af265786898fa
akash-shashi <a.shashi.chaurasia@accenture.com>
2025-08-07 15:27:41 +0530

    updated the comment

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,7 +1,7 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
+  /* change to ul, li test message*/
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
